a22d02a75d250d55ad52d930655c0f4be48cbc06
Chunjie Lu <lvchunjie00@gmail.com>
2018-11-02 23:23:20 -0400

    average

---
+++ read.py
@@ -0,0 +1,14 @@
+data = []
+count = 0
+with open ('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print(sum_len/len(data))
+
