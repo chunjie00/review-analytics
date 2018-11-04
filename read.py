835423e1c5a93514e4ca9c638edac665ac5a1270
Chunjie Lu <lvchunjie00@gmail.com>
2018-11-03 22:36:50 -0400

    add flitering

--- read.py
+++ read.py
@@ -5,10 +5,16 @@ with open ('reviews.txt','r') as f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print(len(data))
+			print('共有', len(data), '笔留言')
 
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-print(sum_len/len(data))
+print('留言的平均长度为', sum_len/len(data), '字')
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言小于100')
+print(new[0])
